3940c24965d1dbc6e580412b68a387ab0e008bc2
Suvo <subrato91ne@gmail.com>
2025-09-22 16:27:52 +0000

    Delete some file

--- drop.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-    mysql -u root -e "UPDATE asterisk.vicidial_campaign_stats SET drops_today = '0' WHERE campaign_id = 'WEBAIDUS';"
-    sleep 1
-done
--- dropper.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-    mysql -u root -e "UPDATE asterisk.vicidial_campaign_stats SET drops_answers_today_pct = '1.02' WHERE campaign_id = 'WEBAIDUS';"
-    sleep 1
-done
